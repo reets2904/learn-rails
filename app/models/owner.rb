97844e2bb53b25b2d3f6e859373ddb959206d52b
Reetam Mitra <reetam.mitra@hiode.com>
2014-02-01 18:02:07 +0530

    dynamic home page

---
+++ app/models/owner.rb
@@ -0,0 +1,18 @@
+class Owner
+def name
+name = 'Reetam Mitra'
+end
+def birthdate
+birthdate = Date.new(1988, 4, 29)
+end
+def countdown
+today = Date.today
+birthday = Date.new(today.year, birthdate.month, birthdate.day)
+if birthday > today
+countdown = (birthday - today).to_i
+else
+countdown = (birthday.next_year - today).to_i
+end
+end
+end
+
